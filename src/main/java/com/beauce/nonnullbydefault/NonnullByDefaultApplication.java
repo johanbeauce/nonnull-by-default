147abb3615624e407428dbaf42d86b538167537c
Johan Beaucé <johan.beauce@gmail.com>
2025-05-10 23:03:47 +0200

    create spring boot project

---
+++ src/main/java/com/beauce/nonnullbydefault/NonnullByDefaultApplication.java
@@ -0,0 +1,13 @@
+package com.beauce.nonnullbydefault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonnullByDefaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonnullByDefaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beauce/nonnullbydefault/NonnullByDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauce.nonnullbydefault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonnullByDefaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
